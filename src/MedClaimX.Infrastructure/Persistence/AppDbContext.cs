c64d580b0d78bbc4e4b08f0956df0d46d0a76ed1
dmckay <dmckay@managewaresolutions.com>
2025-04-11 06:48:19 -0700

    DM - MedClaimX - Init Commit

---
+++ src/MedClaimX.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MedClaimX.Core.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedClaimX.Infrastructure.Persistence
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
